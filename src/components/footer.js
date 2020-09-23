788fdb96c215fe7da1b832615194f7d69165ed7e
Catherine Pollock <pollockcr@gmail.com>
2020-09-22 22:28:27 -0400

    style logo; add featured font

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { RiHeart2Line } from "react-icons/ri";
+import { FaCat, FaDog } from "react-icons/fa";
 
 const Footer = () => (
 	<footer className="site-footer">
-		<div className="mb-3">
+		<div className="mb-5p position-relative">
 			<div
 				class="fb-comments"
 				data-href="https://ellasdaydream.com"
@@ -12,38 +13,46 @@ const Footer = () => (
 			></div>
 		</div>
 		<div className="container">
-			<p>
-				"To know Ella was to love her." Made with{" "}
-				<span className="icon -love">
-					<RiHeart2Line />
-				</span>{" "}
-				Ella's friends and family. Website by{" "}
+			<div className="catdog">
 				<a
 					href="https://catherinepollock.com/"
 					target="_blank"
 					rel="noreferrer"
+					className="catdog-link"
 				>
-					Catherine Pollock
+					<FaCat />
+					<FaDog />
 				</a>
-				.
-			</p>
-			<p>
-				<a
-					href="https://www.instagram.com/ellasdaydream/"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Ella's Daydream Instagram
-				</a>{" "}
-				|{" "}
-				<a
-					href="https://www.instagram.com/_ellayay/"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Ella's Personal Instagram
-				</a>
-			</p>
+			</div>
+			<div className="footer-content position-relative">
+				<p>
+					{" "}
+					<span className="icon -love">
+						<RiHeart2Line />
+					</span>{" "}
+					"To know Ella was to love her."{" "}
+					<span className="icon -love">
+						<RiHeart2Line />
+					</span>
+				</p>
+				<p>
+					<a
+						href="https://www.instagram.com/ellasdaydream/"
+						target="_blank"
+						rel="noreferrer"
+					>
+						Ella's Daydream Instagram
+					</a>{" "}
+					|{" "}
+					<a
+						href="https://www.instagram.com/_ellayay/"
+						target="_blank"
+						rel="noreferrer"
+					>
+						Ella's Personal Instagram
+					</a>
+				</p>
+			</div>
 		</div>
 	</footer>
 );
--- src/components/logo.js
+++ src/components/logo.js
@@ -1,10 +1,12 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Link } from "gatsby";
 
 const Logo = (props) => (
-  <div className="site-logo">
-    <Link to="/">{props.title}</Link>
-  </div>
-)
+	<div className="site-logo font-secondary">
+		<Link to="/" className="site-logo-link">
+			<span className="site-logo-overlay" aria-hidden>{props.title}</span> {props.title}
+		</Link>
+	</div>
+);
 
-export default Logo
+export default Logo;
