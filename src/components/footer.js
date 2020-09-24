251904eb7e97c1a2ca9095eef0a5a05e5b3347f8
Catherine Pollock <pollockcr@gmail.com>
2020-09-24 18:46:18 -0400

    fix footer padding and alignment

--- src/components/footer.js
+++ src/components/footer.js
@@ -14,72 +14,71 @@ const Footer = () => (
 				data-width=""
 			></div>
 		</div>
-		<div className="container">
-			<div className="catdog">
+
+		<div className="catdog">
+			<a
+				href="https://catherinepollock.com/"
+				target="_blank"
+				rel="noreferrer"
+				className="catdog-link"
+				title="catdog"
+			>
+				<FaCat />
+				<FaDog />
+			</a>
+		</div>
+		<div className="footer-content position-relative">
+			<p>
+				{" "}
+				<span className="icon -love">
+					<RiHeart2Line />
+				</span>{" "}
+				"To know Ella was to love her"{" "}
+				<span className="icon -love">
+					<RiHeart2Line />
+				</span>
+			</p>
+			<p>
 				<a
-					href="https://catherinepollock.com/"
+					href="https://www.instagram.com/ellasdaydream/"
 					target="_blank"
 					rel="noreferrer"
-					className="catdog-link"
-					title="catdog"
 				>
-					<FaCat />
-					<FaDog />
+					<IoLogoInstagram /> @ellasdaydream
 				</a>
-			</div>
-			<div className="footer-content position-relative">
-				<p>
-					{" "}
-					<span className="icon -love">
-						<RiHeart2Line />
-					</span>{" "}
-					"To know Ella was to love her"{" "}
-					<span className="icon -love">
-						<RiHeart2Line />
-					</span>
-				</p>
-				<p>
-					<a
-						href="https://www.instagram.com/ellasdaydream/"
-						target="_blank"
-						rel="noreferrer"
-					>
-						<IoLogoInstagram /> @ellasdaydream
-					</a>
-				</p>
-				<p>
-					<a
-						href="https://www.instagram.com/_ellayay/"
-						target="_blank"
-						rel="noreferrer"
-					>
-						<IoLogoInstagram /> @_ellayay
-					</a>
-				</p>
-				<p>
-					<Link to="/posts">View Posts</Link>
-				</p>
-				<p>
-					<Link to="/share">Create Post</Link>
-				</p>
-				<p className="font-size-sm">
-					<a
-						href="https://catherinepollock.com/privacy-policy"
-						target="_blank"
-						rel="noreferrer"
-					>
-						Privacy Policy
-					</a>{" "}
-					~{" "}
-					<a
-						href="https://catherinepollock.com/terms-of-service"
-						target="_blank"
-						rel="noreferrer"
-					>
-						Terms and Conditions
-					</a>
-				</p>
-			</div>
+			</p>
+			<p>
+				<a
+					href="https://www.instagram.com/_ellayay/"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<IoLogoInstagram /> @_ellayay
+				</a>
+			</p>
+			<p>
+				<Link to="/posts">View Posts</Link>
+			</p>
+			<p>
+				<Link to="/share">Create Post</Link>
+			</p>
+			<p className="font-size-sm">
+				<a
+					href="https://catherinepollock.com/privacy-policy"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Privacy Policy
+				</a>{" "}
+				~{" "}
+				<a
+					href="https://catherinepollock.com/terms-of-service"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Terms and Conditions
+				</a>
+			</p>
 		</div>
 	</footer>
 );
