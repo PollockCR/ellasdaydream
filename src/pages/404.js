a769e8253bac6a6d84803be9b3927a586e6b10af
Catherine Pollock <pollockcr@gmail.com>
2020-09-28 10:20:12 -0400

    customize 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,28 +1,42 @@
-import React from "react"
-import { Link } from "gatsby"
-import {RiArrowLeftSLine, RiBugLine, RiSkullLine} from "react-icons/ri"
+import React from "react";
+import { Link } from "gatsby";
+import { RiArrowLeftSLine, RiBugLine } from "react-icons/ri";
+import { AiOutlineQuestion } from "react-icons/ai";
+import { BiHomeHeart } from "react-icons/bi";
 
-import SEO from "../components/seo"
-import Layout from "../components/layout"
+import SEO from "../components/seo";
+import Layout from "../components/layout";
 
 const NotFound = () => (
-  <Layout className="not-found-page">
-    <SEO title="Page not found"/>
-    <div className="wrapper" style={{
-      textAlign: "center"
-    }}>
-      <header>
-        <RiSkullLine style={{
-          fontSize: "128px",
-          color: "var(--primary-color)"
-        }}/>
-        <h1>Oops we did not expect that to happen</h1>
-        <p>You have wondered into the unknown.</p>
-      </header>
-      <Link to="/" className="button"><RiArrowLeftSLine className="icon -left"/>Back to Homepage</Link>
-      <Link to="/share" className="button -outline">Report this <RiBugLine className="icon -right"/></Link>
-    </div>
-  </Layout>
-)
+	<Layout className="not-found-page">
+		<SEO title="Page not found" />
+		<div
+			className="wrapper"
+			style={{
+				textAlign: "center",
+			}}
+		>
+			<header>
+				<AiOutlineQuestion
+					style={{
+						fontSize: "128px",
+						color: "var(--primary-color)",
+					}}
+				/>
+				<h1>Oops - we did not expect that to happen</h1>
+				<p>You have wondered into the unknown.</p>
+			</header>
+			<p>
+				<Link to="/" className="button mr-3 mb-3">
+					<RiArrowLeftSLine className="icon -left" />
+					<BiHomeHeart className="icon -left" /> Back to Homepage
+				</Link>
+				<Link to="/share" className="button -outline">
+					<RiBugLine className="icon -left" /> Report this
+				</Link>
+			</p>
+		</div>
+	</Layout>
+);
 
-export default NotFound
+export default NotFound;
