fb1b0f107f3f93cb2542aea335a5adee47bda3dc
Catherine Pollock <pollockcr@gmail.com>
2020-09-30 17:36:05 -0400

    update thanks verbage

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -16,7 +16,7 @@ const Thanks = () => (
         color: "var(--primary-color)"
       }}/>
       <h1>Got your message</h1>
-      <p>Thank you for sharing. We received your submission and will post it soon.</p>
+      <p>Thank you for sharing. We received your submission and will post it within the next few days.</p>
       <Link to="/" className="button"><RiArrowLeftSLine className="icon -left"/>Home</Link>
     </div>
 
